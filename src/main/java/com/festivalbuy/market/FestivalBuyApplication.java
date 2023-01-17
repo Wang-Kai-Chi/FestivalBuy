50562c5f6481463cd43e7f9829a4dae28feb69c8
Eric Wang <ericwangcatch@gmail.com>
2023-01-17 14:18:45 +0800

    initial commit

---
+++ src/main/java/com/festivalbuy/market/FestivalBuyApplication.java
@@ -0,0 +1,13 @@
+package com.festivalbuy.market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FestivalBuyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FestivalBuyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/festivalbuy/market/FestivalBuyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.festivalbuy.market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FestivalBuyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
