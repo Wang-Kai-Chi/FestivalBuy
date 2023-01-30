bb1370ad3e06ae21cf9c66a3b73e1a86af370f54
Eric Wang <ericwangcatch@gmail.com>
2023-01-30 11:41:09 +0800

    update order_tracking, add orderlist.json

--- src/main/resources/static/js/ShoppingCart.js
+++ src/main/resources/static/js/ShoppingCart.js
@@ -4,6 +4,14 @@ const cartTable = document.getElementById("cart-body")
 
 function refreshTable(table, orders) {
     for (let i of orders) {
+        let info = i
+        let data = {
+            details: `<img src="${info.pic}" width="120">` + info.name,
+            price: info.price,
+            quantity: info.quantity,
+            inStock: info.inStock,
+            changeBtn: `<button class="btn btn-primary" id="${'deleteBtn' + info.id}">刪除</button>`,
+        }
         const obj = {
             details: null,
             price: null,
@@ -11,17 +19,14 @@ function refreshTable(table, orders) {
             inStock: null,
             changeBtn: null
         }
-        const data = {
-            details: `<img src="${i.pic}" width="120">` + i.name,
-            price: i.price,
-            quantity: i.quantity,
-            inStock: i.inStock,
-            changeBtn: `<button id="${'deleteBtn'+i.id}">刪除</button>`,
-        }
         if (table.rows.length < orders.length) {
             appendData(table, obj, data)
-        }else{
-            updateTable(table, data)
+        }
+        else if(table.rows.length > 0){
+            for(const i in table.rows)
+                table.deleteRow(i)
+                
+            appendData(table, obj, data)
         }
     }
 }
@@ -29,13 +34,13 @@ function refreshTable(table, orders) {
 function appendData(table, obj, data) {
     const row = table.insertRow()
 
-    for (let i in obj){
+    for (let i in obj) {
         obj[i] = row.insertCell()
         obj[i].innerHTML = data[i]
     }
 }
 
-function updateOrderDetail(orderDetail, products){
+function updateOrderDetail(orderDetail, products) {
     for (let i of products) {
         orderDetail.itemQuantity++
         orderDetail.totalQuantity += parseInt(i.quantity)
@@ -44,19 +49,19 @@ function updateOrderDetail(orderDetail, products){
 }
 
 function showOrderDetail(orderDetail) {
+    updateOrderDetail(orders.orderDetail, orders.list)
+
     const component = {
-        itemQuantity:document.getElementById("item-quantity"),
-        totalQuantity:document.getElementById("total-quantity"),
-        subTotal:document.getElementById("sub-total")
+        itemQuantity: document.getElementById("item-quantity"),
+        totalQuantity: document.getElementById("total-quantity"),
+        subTotal: document.getElementById("sub-total")
     }
 
-    for(let i in orderDetail)
+    for (let i in orderDetail)
         component[i].innerHTML = orderDetail[i]
 }
 
 refreshTable(cartTable, orders.list)
-
-updateOrderDetail(orders.orderDetail, orders.list)
 
 showOrderDetail(orders.orderDetail)
 
@@ -64,30 +69,30 @@ setEvenListenerToDeleteBtns()
 
 document.getElementById("delete-all").onclick = clearCart
 
-updateTable(cartTable)
-
-function updateTable(table, data){
-    for(let r = 0;r<table.rows.length;r++){
-        let row = table.rows[r]
-        for(let c = 0;c<row.cells.length;c++){
-            let cell = row.cells[c]
+function updateTable(table, data) {
+    for (const element of table.rows) {
+        let row = element
+        for (const element of row.cells) {
+            let cell = element
             console.log(cell.innerHTML)
         }
     }
 }
 
-function clearCart(){
+function clearCart() {
     console.log(orders.list)
-    refreshTable(cartTable, orders.list)
 }
 
-function setEvenListenerToDeleteBtns(){
-    for(let i = 1;i<=cartTable.rows.length;i++){
-        const btn = document.getElementById("deleteBtn"+i)
+function setEvenListenerToDeleteBtns() {
+    for (let i = 1; i <= cartTable.rows.length; i++) {
+        const btn = document.getElementById("deleteBtn" + i)
 
-        btn.onclick = ()=>{
-            console.log(orders.list[i-1])
-            refreshTable(cartTable, orders.list)
+        btn.onclick = () => {
+            for (const j in orders.list) {
+                if (orders.list[j].id == i){
+                    console.log(orders.list[j])
+                }
+            }
         }
     }
 }
