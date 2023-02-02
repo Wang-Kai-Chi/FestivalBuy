34278fe6e54e9d60760064910ad03562f42d9619
Eric Wang <ericwangcatch@gmail.com>
2023-02-02 11:11:26 +0800

    update FrontPage.js

--- src/main/resources/static/js/FrontPage.js
+++ src/main/resources/static/js/FrontPage.js
@@ -1,6 +1,18 @@
 const dollar = "$"
 const products = []
 
+main()
+
+function main() {
+    window.onload = () => {
+        let url = "/api/products"
+        fetch(url)
+            .then(data => data.json())
+            .then(value => showData(value))
+            .catch(() => console.log("fetch fail"))
+    }
+}
+
 function showData(value) {
     products.push(value[1], value[2], value[3])
 
@@ -12,24 +24,24 @@ function showData(value) {
             <div class="card-body">
             <h5 class="card-title">${product.title}</h5>
             <p class="card-text">${dollar + product.price}</p>
-            <button id="${'gobtn-'+i}" class="btn btn-primary">購買</button>
+            <a class="${'gobtn-' + i} btn btn-primary">購買</a>
             </div>
         </div>
         </div>`
         document.querySelector("#hot-product").innerHTML += hotProduct
-        console.log(i)
-        console.log(document.getElementById("gobtn-0"))
     }
+
+    setBtnListener()
 }
 
-function saveCookie() {
-    console.log("click")
-    // document.cookie = `current_product=${products[0].product_id}`
-}
-
-window.onload = () => {
-    let url = "/api/products"
-    fetch(url)
-        .then(data => data.json())
-        .then(value => showData(value))
+function setBtnListener() {
+    document.querySelector(".gobtn-0").onclick = () => {
+        console.log("click")
+    }
+    document.querySelector(".gobtn-1").onclick = () => {
+        console.log("click")
+    }
+    document.querySelector(".gobtn-2").onclick = () => {
+        console.log("click")
+    }
 }
