7a715f4f74461f4cb1267c08bb39e04342ae0e1d
Eric Wang <ericwangcatch@gmail.com>
2023-02-06 12:03:54 +0800

    update setOrder in ProductPage.js(unable to save orderDetail to local)

--- src/main/resources/static/js/ProductPage.js
+++ src/main/resources/static/js/ProductPage.js
@@ -1,41 +1,83 @@
-import * as cookie from "./ParseCookie.js"
+import * as cookieParser from "./ParseCookie.js"
 import * as sc from "./StringCollection.js"
+
+let orderDetail = {
+    product: {},
+    customer: {},
+    quantity: null,
+    subtotal: null
+}
 
 main()
 
 function main() {
     window.onload = () => {
-        const cookieObj = cookie.parseCookie(document.cookie)
+        const cookieObj = cookieParser.parseCookie(document.cookie)
 
         console.log(cookieObj)
 
-        let url = "/api/products/" + cookieObj.current_product
-        fetch(url)
-            .then(data => data.json())
-            .then(value => showData(value))
+        fetchData(cookieObj)
     }
 }
 
-const showData = value => {
-    const product = value
+const setOrder = productData => {
+    console.log(productData)
+    const eQuantity = document.querySelector(sc.productId.quantity)
+    eQuantity.value = Math.min(Math.max(eQuantity.min, eQuantity.value), eQuantity.max)
+    
+    let quantity = eQuantity.value
+    
+    let subtotal = quantity * productData.price
 
-    document.querySelector(sc.productId.name).innerHTML = product.title
-    document.querySelector(sc.productId.price).innerHTML = sc.dollar + product.price
-    document.querySelector(sc.productId.descripion).innerHTML = product.description
+    orderDetail = {
+        product: productData,
+        quantity: quantity,
+        subtotal: subtotal
+    }
 
-    document.querySelector(sc.productId.img).src = product.imageurl
-    document.querySelector(sc.productId.quantity).value = 0
+    setBuyBtnListener(orderDetail)
+}
 
-    document.querySelector(sc.productId.btn).onclick = () => {
-        saveStorage(value)
+async function fetchData(cookieObj) {
+    let url = "/api/products/" + cookieObj.current_product
+    fetch(url)
+        .then(data => data.json())
+        .then(value => parseValue(value))
+        .catch(err => console.log(err))
+}
+
+const parseValue = value => {
+    document.querySelector(sc.productId.name).innerHTML = value.title
+    document.querySelector(sc.productId.price).innerHTML = sc.dollar + value.price
+    document.querySelector(sc.productId.descripion).innerHTML = value.description
+
+    document.querySelector(sc.productId.img).src = value.imageurl
+
+    setOrder(value)
+    document.querySelector(sc.productId.quantity).onchange = () => {
+        setOrder(value)
+    }
+}
+
+const setBuyBtnListener = order => {
+    const btn = document.querySelector(sc.productId.btn)
+    btn.onclick = () => {
+        btn.classList.toggle('btn-warning')
+
+        btn.innerHTML = "已購買"
+
+        document.querySelector(sc.productId.quantity).remove()
+        document.querySelector(".qtext").remove()
+
+        saveStorage(order)
         console.log(loadStorage())
     }
 }
 
 const saveStorage = data =>
-    localStorage.setItem("product", JSON.stringify(data))
+    localStorage.setItem("order", JSON.stringify(data))
 
 const loadStorage = () => {
-    let product = localStorage.getItem("product")
+    let product = localStorage.getItem("order")
     return JSON.parse(product)
 }
