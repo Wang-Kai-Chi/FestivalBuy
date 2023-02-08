4d71f42e224b1945a90032593a46d8a9601fc943
Eric Wang <ericwangcatch@gmail.com>
2023-02-08 15:33:18 +0800

    add Get function getOrderDetailByCustomerId to Controller

--- src/main/java/com/festivalbuy/market/controller/OrderDetailController.java
+++ src/main/java/com/festivalbuy/market/controller/OrderDetailController.java
@@ -3,10 +3,13 @@ package com.festivalbuy.market.controller;
 import java.util.ArrayList;
 import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.festivalbuy.market.entity.OrderDetail;
@@ -20,21 +23,62 @@ import com.festivalbuy.market.repo.ProductRepository;
 @RestController
 @RequestMapping("/api/order_details")
 public class OrderDetailController {
+	@Autowired
 	private OrderDetailRepository orderDetailRepo;
+	@Autowired
 	private ProductRepository productRepo;
+	@Autowired
 	private ProductOrderRepository productOrderRepo;
-
-	public OrderDetailController(OrderDetailRepository orderDetailRepo, ProductRepository productRepo,
-			ProductOrderRepository productOrderRepo) {
-		super();
-		this.orderDetailRepo = orderDetailRepo;
-		this.productRepo = productRepo;
-		this.productOrderRepo = productOrderRepo;
-	}
 
 	@GetMapping
 	Iterable<OrderDetail> getOrderDetails() {
 		return orderDetailRepo.findAll();
+	}
+
+	@GetMapping("/customer/{customerId}")
+	ArrayList<OrderDetail> getOrderDetailByCustomerId(@PathVariable Integer customerId) {
+
+		return getOrderDetailWithSameCustomer(customerId);
+	}
+
+	ArrayList<OrderDetail> getOrderDetailWithSameCustomer(Integer customerId) {
+		ArrayList<ProductOrder> orderList = getOrdersWithSameCustomer(customerId);
+
+		ArrayList<OrderDetail> detailList = (ArrayList<OrderDetail>) orderDetailRepo.findAll();
+		ArrayList<OrderDetail> temp = new ArrayList<>();
+
+		for (ProductOrder p : orderList) {
+			for (OrderDetail o : detailList) {
+				if (p == o.getOrderDetailKey().getProductOrder())
+					temp.add(o);
+			}
+		}
+		return temp;
+	}
+
+	public ArrayList<ProductOrder> getOrdersWithSameCustomer(Integer customerId) {
+		ArrayList<ProductOrder> orderList = (ArrayList<ProductOrder>) productOrderRepo.findAll();
+		ArrayList<ProductOrder> temp = new ArrayList<>();
+
+		for (ProductOrder p : orderList) {
+			if (p.getCustomer().getCustomer_id() == customerId) {
+				Integer orderId = p.getOrder_id();
+
+				temp.add(getOrderWithKey(orderId));
+			}
+		}
+
+		return temp;
+	}
+
+	@GetMapping("/{orderId}/{productId}")
+	Optional<OrderDetail> getOrderDetailByCompositeId(@PathVariable Integer orderId, @PathVariable Integer productId) {
+		OrderDetailKey orderDetailKey = new OrderDetailKey();
+
+		orderDetailKey.setProductOrder(getOrderWithKey(orderId));
+		orderDetailKey.setProduct(getProductWithKey(productId));
+
+		return orderDetailRepo.findById(orderDetailKey);
 	}
 
 	@PostMapping
@@ -42,10 +86,11 @@ public class OrderDetailController {
 		ArrayList<OrderDetail> detailList = (ArrayList<OrderDetail>) orderDetails;
 
 		for (OrderDetail o : detailList) {
+			OrderDetailKey old = o.getOrderDetailKey();
 			OrderDetailKey key = new OrderDetailKey();
 
-			key.setProduct(getProductWithKey(o.getOrderDetailKey()));
-			key.setProductOrder(getOrderWithKey(o.getOrderDetailKey()));
+			key.setProduct(getProductWithKey(old.getProduct().getProduct_id()));
+			key.setProductOrder(getOrderWithKey(old.getProductOrder().getOrder_id()));
 
 			o.setOrderDetailKey(key);
 		}
@@ -53,15 +98,15 @@ public class OrderDetailController {
 		return orderDetailRepo.saveAll(detailList);
 	}
 
-	private ProductOrder getOrderWithKey(OrderDetailKey orderDetailKey) {
-		Optional<ProductOrder> po = productOrderRepo.findById(orderDetailKey.getProductOrder().getOrder_id());
-		
-		return (po.isPresent())?po.get():null;
+	private ProductOrder getOrderWithKey(Integer id) {
+		Optional<ProductOrder> po = productOrderRepo.findById(id);
+
+		return (po.isPresent()) ? po.get() : null;
 
 	}
 
-	private Product getProductWithKey(OrderDetailKey orderDetailKey) {
-		Optional<Product> p = productRepo.findById(orderDetailKey.getProduct().getProduct_id());
-		return (p.isPresent())?p.get():null;
+	private Product getProductWithKey(Integer id) {
+		Optional<Product> p = productRepo.findById(id);
+		return (p.isPresent()) ? p.get() : null;
 	}
 }
--- src/test/java/com/festivalbuy/market/FestivalBuyApplicationTests.java
+++ src/test/java/com/festivalbuy/market/FestivalBuyApplicationTests.java
@@ -1,13 +1,23 @@
 package com.festivalbuy.market;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.festivalbuy.market.controller.OrderDetailController;
 
 @SpringBootTest
 class FestivalBuyApplicationTests {
+	@Autowired
+	OrderDetailController orderDetailController;
 
 	@Test
 	void contextLoads() {
+	}
+	
+	@Test
+	void testGetOrdersWithSameCustomer() {
+		System.out.println(orderDetailController.getOrdersWithSameCustomer(2));
 	}
 
 }
