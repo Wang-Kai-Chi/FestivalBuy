430197288c568e0c5487ed0d86f8fa1973162afb
Eric Wang <ericwangcatch@gmail.com>
2023-02-10 15:44:16 +0800

    add login function to CustomerController

--- src/test/java/com/festivalbuy/market/FestivalBuyApplicationTests.java
+++ src/test/java/com/festivalbuy/market/FestivalBuyApplicationTests.java
@@ -4,20 +4,24 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.festivalbuy.market.controller.CustomerController;
 import com.festivalbuy.market.controller.OrderDetailController;
 
 @SpringBootTest
 class FestivalBuyApplicationTests {
 	@Autowired
 	OrderDetailController orderDetailController;
-
-	@Test
-	void contextLoads() {
-	}
+	@Autowired
+	CustomerController customerController;
 	
-	@Test
 	void printData() {
 		System.out.println(orderDetailController.getOrderDetailsWithSameCustomer(1));
 	}
 
+	@Test
+	void testCustomerEmail() {
+		assert(customerController.checkPassword(
+				customerController.getCustomerById(1).get(), 
+				customerController.findCustomerByEmail("food@gmail.com")));
+	}
 }
