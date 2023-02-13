d5aef4b8574425c5d12beb7022010def2ae3fd6b
Eric Wang <ericwangcatch@gmail.com>
2023-02-13 16:36:36 +0800

    add Dialogflow controller and config

---
+++ src/main/java/com/festivalbuy/market/DialogflowConfig.java
@@ -0,0 +1,13 @@
+package com.festivalbuy.market;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DialogflowConfig {
+	
+//	@Bean
+//	GsonFactory gsonFactory() {
+//	    return GsonFactory.getDefaultInstance();
+//	}
+}
