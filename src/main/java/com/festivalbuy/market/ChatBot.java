a735ff936ad4d59f42332d987aa782e463efe8fe
Eric Wang <ericwangcatch@gmail.com>
2023-02-14 10:45:22 +0800

    update ChatBot(show all products)

--- src/main/java/com/festivalbuy/market/ChatBot.java
+++ src/main/java/com/festivalbuy/market/ChatBot.java
@@ -1,19 +1,36 @@
 package com.festivalbuy.market;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.actions.api.ActionRequest;
 import com.google.actions.api.ActionResponse;
 import com.google.actions.api.DialogflowApp;
 import com.google.actions.api.ForIntent;
 import com.google.actions.api.response.ResponseBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component
 public class ChatBot extends DialogflowApp {
-    @Autowired
+	@Autowired
     private ProductService productService;
 
-    @ForIntent("sa.agent.products")
+    @ForIntent("FindProductByCategory")
     public ActionResponse product(ActionRequest request) {
-        ResponseBuilder responseBuilder = getResponseBuilder(request).add(productService.getProductList().toString());
+    	System.out.println(request.getParameter("category"));
+        ResponseBuilder responseBuilder = getResponseBuilder(request).add(getResponseJson());
         return responseBuilder.build();
+    }
+    
+    String getResponseJson() {
+    	ObjectMapper mapper = new ObjectMapper();
+    	String result = "";
+        try {
+			result = mapper.writeValueAsString(productService.getProductList());
+		} catch (JsonProcessingException e) {
+			e.printStackTrace();
+		}
+        
+        return result;
     }
 }
--- src/main/java/com/festivalbuy/market/controller/DialogflowWebhookController.java
+++ src/main/java/com/festivalbuy/market/controller/DialogflowWebhookController.java
@@ -2,29 +2,35 @@ package com.festivalbuy.market.controller;
 
 import com.festivalbuy.market.ChatBot;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 import java.util.concurrent.ExecutionException;
 
+@RestController
+@RequestMapping("/dialogflow/webhook")
 public class DialogflowWebhookController {
     @Autowired
     private ChatBot chatBot;
 
-    @RequestMapping(value = "/", method = RequestMethod.POST, produces = { "application/json" })
+    @PostMapping()
     String serveAction(@RequestBody String body, @RequestHeader Map<String, String> headers) {
         try {
             return chatBot.handleRequest(body, headers).get();
-        } catch (InterruptedException | ExecutionException e) {
+        } catch (InterruptedException e) {
             return handleError(e);
-        }
+        }catch (ExecutionException e) {
+        	return handleError(e);
+		}
     }
 
     private String handleError(Exception e) {
         e.printStackTrace();
+        System.out.println("Error in App.handleRequest "+e);
         return "Error handling the intent - " + e.getMessage();
     }
 }
