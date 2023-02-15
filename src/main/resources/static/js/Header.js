fe0177706413836dee316561952b501ed8957b3d
Eric Wang <ericwangcatch@gmail.com>
2023-02-15 15:33:48 +0800

    update Header.js and views

--- src/main/resources/static/js/Header.js
+++ src/main/resources/static/js/Header.js
@@ -32,18 +32,35 @@ function checkLoginState() {
         console.log(cookie)
 
         if (cookie.customer_id != null) {
-            document.querySelector("#header-user-action").innerHTML = userMugShot(cookie.full_name)
+            document.querySelector("#header-user-action").innerHTML = ""
+            document.querySelector(".nav").innerHTML = userDropDown(cookie.full_name)
 
             console.log(document.querySelector("#header-user-action").innerHTML)
 
             document.querySelector("#logout-btn").onclick = () => handleLogout(userCookie)
+
+            document.querySelector("#user-dropdown").onclick = () => {
+                const menu = document.querySelector(".dropdown-menu")
+
+                if (menu.classList.contains('show'))
+                    menu.classList.remove('show')
+                else
+                    menu.classList.add('show')
+            }
         }
     }
 }
 
-const userMugShot = (name) => {
-    return `<span class="text-light me-2">你好!${name}</span>
-    <button type="button" id="logout-btn" class="btn btn-danger" onclick="">登出</button>`
+const userDropDown = (name) => {
+    return `<li class="nav-item dropdown">
+    <a class="nav-link dropdown-toggle text-light" href="#" id="user-dropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">歡迎光臨! ${name}</a>
+    <div class="dropdown-menu" aria-labelledby="dropdown01">
+      <a class="dropdown-item" href="/shopping_cart">購物車</a>
+      <a class="dropdown-item" href="/order_tracking">訂單查詢</a>
+      <hr>
+      <a class="dropdown-item text-danger" href="/" id="logout-btn">登出</a>
+    </div>
+  </li>`
 }
 
 function handleLogout(userCookie) {
